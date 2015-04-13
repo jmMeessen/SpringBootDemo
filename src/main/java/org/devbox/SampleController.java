4d0486cae6731c1c8721190efed60df08c5437c1
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2015-04-13 22:21:41 +0200

    First shot at the demo program

---
+++ src/main/java/org/devbox/SampleController.java
@@ -0,0 +1,24 @@
+package hello;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+    	String str = "<center><h1><br/>Hello MIX-IT!</h1>";
+    	str = str + "<br/><h2>Happy to be in Lyon.</h2></center>";
+        return str;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
+
